bd4483e730adaf169fa4b556ffa3b428281990d9
Sandro Alvarez and Stephen Burden <sandro+stephen@>
2016-10-24 16:40:39 -0700

    add navbar

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,4 +1,15 @@
 import FirebaseAdapter from 'emberfire/adapters/firebase';
 
 export default FirebaseAdapter.extend({
+  model() {
+    return Ember.RSVP.hash({
+      rentals: this.store.findAll('rental'),
+      announcements: this.store.findAll('announcement')
+    });
+  },
+
+  setupController(controller, models) {
+   controller.set('rentals', models.rentals);
+   controller.set('announcements', models.announcements);
+  }
 });
